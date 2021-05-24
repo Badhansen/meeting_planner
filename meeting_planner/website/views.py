c52e56f69e52fc4eaff1e412f5dc9d3e1f60a516
Badhan Sen <badhan.cse@gmail.com>
2021-05-24 20:23:49 +0600

    added flake8 ignore command

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from datetime import datetime
 
 
 def welcome(request):
     return HttpResponse("Welcome to the Meeting planner!")
+
+
+def date(request):
+    return HttpResponse("The page was served at " + str(datetime.now()))
+
+
+def about(request):
+    return HttpResponse("I am Badhan Sen.")
